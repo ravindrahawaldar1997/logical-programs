9e93d3605aaf6ecfa907cbed28e1c205259dec53
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-28 21:33:52 +0530

    Added: Program to convert integer to binary number

--- src/main/java/com/bridgelabz/logical_program_two/LogicalPrograms.java
+++ src/main/java/com/bridgelabz/logical_program_two/LogicalPrograms.java
@@ -46,6 +46,7 @@ public class LogicalPrograms {
         Scanner scan = new Scanner(System.in);
         return scan.nextInt();
     }
+
     static double squareRoot() {
         System.out.println("Enter positive Integer value ");
         double c = LogicalPrograms.getIntValue();
@@ -58,7 +59,8 @@ public class LogicalPrograms {
         System.out.println(t);
         return t;
     }
-    static void toBinary(){
+
+    static void toBinary() {
         int num;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter number to convert in binary ");
@@ -79,11 +81,53 @@ public class LogicalPrograms {
         }
         System.out.println("Padding of Binary is " + binary);
     }
+
+    static void binary() {
+        int num;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number to convert in binary ");
+        num = scan.nextInt();
+        String binary = "";
+        String binarySwap = "";
+        int i = 0;
+        int num1;
+        while (num > 0) {
+            num1 = num % 2;
+            binary = num1 + binary;
+            num = num / 2;
+            i++;
+        }
+        System.out.println(binary);
+        while (binary.length() < 8) {
+
+            binary = "0" + binary;
+        }
+        System.out.println("Padding of Binary is " + binary);
+        String nibble1 = binary.substring(0, 4);
+        String nibble2 = binary.substring(4, binary.length());
+
+        binarySwap = nibble2 + nibble1;
+        System.out.println("After swapping nibbles new number is " + binarySwap);
+        int result = Integer.parseInt(binarySwap);
+        int decimal = 0;
+        int pow = 0;
+
+        while (result > 0) {
+            int rem = result % 10;
+            decimal = (int) (decimal + rem * Math.pow(2, pow));
+            result = result / 10;
+            pow++;
+        }
+        System.out.println("Decimal number for " + binarySwap + " is " + decimal);
+
+    }
+
     public static void main(String[] args) {
-        LogicalPrograms logicalPrograms =new LogicalPrograms();
+        LogicalPrograms logicalPrograms = new LogicalPrograms();
         logicalPrograms.vendingMachine();
         squareRoot();
         toBinary();
+        binary();
 
     }
 }
