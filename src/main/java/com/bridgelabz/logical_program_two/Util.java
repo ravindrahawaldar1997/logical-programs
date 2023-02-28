8fa2aebe4d847a6378988a1d28104267d270b3c3
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-28 19:06:51 +0530

    Added: Program for temperature conversion

--- src/main/java/com/bridgelabz/logical_program_two/Util.java
+++ src/main/java/com/bridgelabz/logical_program_two/Util.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.logical_program_two;
 
+import java.util.Scanner;
+
 public class Util {
     public static int dayOfWeek(int m, int d, int y) {
         int y0 = y - (14 - m) / 12;
@@ -32,12 +34,36 @@ public class Util {
         return d0;
     }
 
+    public static double temperatureConversion(double temperature, String unit) {
+        if (unit.equals("C")) {
+            return (temperature * 9 / 5) + 32;
+        } else if (unit.equals("F")) {
+            return (temperature - 32) * 5 / 9;
+        } else {
+            System.out.println("Invalid unit.");
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]);
         int d = Integer.parseInt(args[1]);
         int y = Integer.parseInt(args[2]);
         int dayOfWeek = Util.dayOfWeek(m, d, y);
         System.out.println(dayOfWeek);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the fahrenheit temperature");
+        double fahrenheitTemp;
+        fahrenheitTemp = sc.nextDouble();
+        double celsiusTemp = org.example.Util.temperatureConversion(fahrenheitTemp, "F");
+        System.out.println(fahrenheitTemp + "°F = " + celsiusTemp + "°C");
+
+        double celsiusTemp2;
+        System.out.println("Enter the celsius temperature");
+        celsiusTemp = sc.nextDouble();
+        double fahrenheitTemp2 = org.example.Util.temperatureConversion(celsiusTemp, "C");
+        System.out.println(celsiusTemp + "°C = " + fahrenheitTemp2 + "°F");
     }
 
 }
