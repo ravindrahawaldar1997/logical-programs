d980d31deb26e5fdf4e4cab98cbb671174fbe8ea
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-28 18:58:09 +0530

    Added: program to find the day of the week

---
+++ src/main/java/com/bridgelabz/logical_program_two/Util.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.logical_program_two;
+
+public class Util {
+    public static int dayOfWeek(int m, int d, int y) {
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + 31 * m0 / 12) % 7;
+        switch (d0) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+        }
+        return d0;
+    }
+
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+        int dayOfWeek = Util.dayOfWeek(m, d, y);
+        System.out.println(dayOfWeek);
+    }
+
+}
+
