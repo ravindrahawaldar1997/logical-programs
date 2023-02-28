1eafc38bae439a8159ded07f8e6e940951d118b7
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-02-28 08:41:59 +0530

    Added: Program to check the number is a prime number or not

--- src/main/java/com/bridgelabz/logical_programs/LogicalPrograms.java
+++ src/main/java/com/bridgelabz/logical_programs/LogicalPrograms.java
@@ -3,8 +3,9 @@ package com.bridgelabz.logical_programs;
 import java.util.Scanner;
 
 public class LogicalPrograms {
+    Scanner sc = new Scanner(System.in);
+
     void fibonacciSeries() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number fibonacci series you want");
         int number = sc.nextInt();
         int a = 0;
@@ -17,20 +18,38 @@ public class LogicalPrograms {
         }
         System.out.println();
     }
-    int perfectNumber(){
-        Scanner sc = new Scanner(System.in);
+
+    int perfectNumber() {
         System.out.println("Enter the number to find is perfect number or not");
         int number = sc.nextInt();
         int perfect = 0;
-        for(int i =1; i<number; i++){
-            if(number % i == 0){
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
                 perfect += i;
             }
         }
-        if(perfect == number){
-            System.out.println(+number +" "  +"is a perfect number ");
-        }else {
-            System.out.println(+number +" " +"is not a perfect number");
+        if (perfect == number) {
+            System.out.println(+number + " " + "is a perfect number ");
+        } else {
+            System.out.println(+number + " " + "is not a perfect number");
+        }
+        return number;
+    }
+
+    int primeNumber() {
+        System.out.println("Enter a number to check it weather is prime number");
+        int number = sc.nextInt();
+        int count = 0;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                count++;
+                break;
+            }
+        }
+        if (count == 0 && number != 1) {
+            System.out.println(+number + " " + "is a prime number");
+        } else {
+            System.out.println(+number + " is not a prime number");
         }
         return number;
     }
@@ -39,5 +58,6 @@ public class LogicalPrograms {
         LogicalPrograms logicalPrograms = new LogicalPrograms();
         logicalPrograms.fibonacciSeries();
         logicalPrograms.perfectNumber();
+        logicalPrograms.primeNumber();
     }
 }
